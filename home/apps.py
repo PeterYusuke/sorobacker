9c57184cf1fbcaec18712b07ab4db39a18253075
Yusuke.ubuntu <yyamashita1201@gmail.com>
2021-03-02 18:42:27 +0900

    clean directory

--- home/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HomeCofnig(AppConfig):
-    name = 'home'
--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home_view
-
-urlpatterns = [
-
-    # this is home view
-    path('', home_view),
-]
