60fb5ba59077d70841046ab1a6b7d4443df16310
Yusuke.ubuntu <yyamashita1201@gmail.com>
2021-04-25 17:42:22 +0900

    whitenoise static file handling

--- development/django_project/django_project/settings.py
+++ development/django_project/django_project/settings.py
@@ -35,6 +35,9 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    # whitenoise app for handling static file
+    'whitenoise.runserver_nostatic',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
