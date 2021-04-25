6ae8ac2068e8706e8d90e4898796be8c0ac46250
Yusuke.ubuntu <yyamashita1201@gmail.com>
2021-04-25 16:39:48 +0900

    whitenoise app included

--- development/django_project/django_project/settings.py
+++ development/django_project/django_project/settings.py
@@ -54,6 +54,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # whitenoise
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django_project.urls'
